1f8af48a219afdd9718ab01baf11a3e25b6e9024
Christian Toledo <chrstiantoledo@gmail.com>
2021-02-24 14:55:05 -0300

    config server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,8 @@
-import express from "express";
-
-const app = express();
+import "reflect-metadata";
+import "./database/index";
+import app from "./app";
 
 const port = 3333;
-
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World in Json" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Dados salvos com sucesso" });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} 🚀`);
